633528c43cd8d9cb5129e30b24ffcbf46e17a516
Hemnath-D <hemnathdhananjayan145@gmail.com>
2020-06-08 18:01:25 +0530

    Added Webhook functionality

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const http = require('http');
 const morgan = require('morgan');
+const webhookRouter = require('./router/webhook');
 
 const app = express()
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+app.use('/webhook', webhookRouter);
 
 const hostname = 'localhost';
 const port = 3000;
